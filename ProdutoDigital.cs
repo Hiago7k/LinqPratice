3ba5e49b29ebafb0d229781da9a3a9adfcfcf623
Hiago 7k <hiago7k@gmail.com>
2026-03-19 19:50:13 -0300

    criando nossa classe de produto

---
+++ ProdutoDigital.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace LinqPratice
+{
+    internal class ProdutoDigital : Produtos 
+    {
+        public ProdutoDigital(string nome, decimal preco, string digital) : base(nome, preco)
+        {
+            Digital = digital;
+        }
+
+        public string Digital { get;  }
+    }
+}
