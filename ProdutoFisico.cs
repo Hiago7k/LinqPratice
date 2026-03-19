382e623caeb484d728a5a1e01c5dbfa42f474fb3
Hiago 7k <hiago7k@gmail.com>
2026-03-19 19:37:02 -0300

    ajustando classe produto fisico

---
+++ ProdutoFisico.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace LinqPratice;
+
+internal class ProdutoFisico : Produtos 
+{
+    public ProdutoFisico(string nome, decimal preco,string tipo) : base(nome, preco)
+    {
+        Tipo = tipo;
+    }
+
+    public string Tipo { get;  }
+}
