622a5b82bdaf5979081043aa630d12af2244af1f
Hiago 7k <hiago7k@gmail.com>
2026-03-19 19:34:10 -0300

    criando classe

---
+++ Produtos.cs
@@ -0,0 +1,21 @@
+﻿
+namespace LinqPratice;
+
+internal class Produtos
+{
+    public Produtos(string nome, decimal preco)
+    {
+        Nome = nome;
+        Preco = preco;
+    }
+
+    public string Nome { get; }
+    public decimal Preco { get; }
+
+
+    public void ExibirDados()
+    {
+        Console.WriteLine(Nome);
+        Console.WriteLine(Preco);
+    }
+}
