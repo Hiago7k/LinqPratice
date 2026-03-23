4dc59b13f76cd8e8c4473ca3cc4fa7db1ed5b2b7
Hiago 7k <hiago7k@gmail.com>
2026-03-23 18:33:35 -0300

    filtrar apenas numeros pares usando linq

--- Program.cs
+++ Program.cs
@@ -3,16 +3,23 @@ using LinqPratice;
 using LinqPratice.Filters;
 
 
-List<Produtos> produtos = new List<Produtos>
+//List<Produtos> produtos = new List<Produtos>
+//{
+//    new  ProdutoFisico("Celular", 2300, "Fisico"),
+//    new ProdutoDigital("Pacote office", 500, "Digital")
+//};
+
+
+//LinqFilter.Filtros(produtos);
+//LinqFilter.Media(produtos);
+
+//Console.WriteLine("Killstrak commits");
+//Console.WriteLine("Killstrak commits");
+//Console.WriteLine("Killstrak commits");
+
+// Atividade 2
+
+List<int> numeros = new List<int> 
 {
-    new  ProdutoFisico("Celular", 2300, "Fisico"),
-    new ProdutoDigital("Pacote office", 500, "Digital")
+    1, 2, 3, 4, 5, 6, 7 ,8 ,0 ,10
 };
-
-
-LinqFilter.Filtros(produtos);
-LinqFilter.Media(produtos);
-
-Console.WriteLine("Killstrak commits");
-Console.WriteLine("Killstrak commits");
-Console.WriteLine("Killstrak commits");
