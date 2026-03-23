c2f2a3efda240c887bd60441519e4cee66402771
Hiago 7k <hiago7k@gmail.com>
2026-03-23 18:43:35 -0300

    criando o método, para realizar a consulta teste

---
+++ Filters/LinqNumeros.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqPratice.Filters;
+
+internal class LinqNumeros
+{
+
+    public static void FiltroInteiros(List<int> numeros)
+    {
+        var filtroDaListaPares = numeros.Where(numeros => numeros > 4).ToList();
+
+        foreach (var numero in filtroDaListaPares) 
+        {
+            Console.WriteLine("Exibindo todos, os números pares...");
+            Console.WriteLine(numero);
+        }
+    }
+   
+}
