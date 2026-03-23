abaa6f9aa4fbef3ee50ff33d74a45479e96486ec
Hiago 7k <hiago7k@gmail.com>
2026-03-23 19:07:33 -0300

    resolvendo desafio exibindo numeros pares

--- Filters/LinqNumeros.cs
+++ Filters/LinqNumeros.cs
@@ -11,12 +11,15 @@ internal class LinqNumeros
 
     public static void FiltroInteiros(List<int> numeros)
     {
-        var filtroDaListaPares = numeros.Where(numeros => numeros > 4).ToList()!;
+        var filtroDaListaPares = numeros.Where(numeros => numeros > 0).ToList()!;
 
         foreach (var numero in filtroDaListaPares)
         {
-            Console.WriteLine("Exibindo todos, os números pares...");
-            Console.WriteLine(numero);
+            if (numero % 2 == 0)
+            {
+                Console.Write("Exibindo numeros pares  ");
+                Console.WriteLine(numero);
+            }   
         }
     }
 
