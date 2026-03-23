d4a87744492eda848fbc7d495f0e61ff6c21c76b
Hiago 7k <hiago7k@gmail.com>
2026-03-23 18:38:56 -0300

    criando classe com a lista

---
+++ Filters/Numeros.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqPratice.Filters;
+
+internal class Numeros
+{
+    public List<int> numeros = new List<int> {1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+}
